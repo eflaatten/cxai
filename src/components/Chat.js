d21b576747f794cfb9b829541523df3f5f166252
eflaatten <elliotflaatten@live.com>
2024-09-03 14:39:27 -0500

    removed old commented code

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -73,26 +73,6 @@ function Chat({ darkTheme, senderMessage, handleSenderMessageChange, handleSendM
     },
   };
 
-  // const components = {
-  //   code({ node, inline, className, children, ...props }) {
-  //     const match = /language-(\w+)/.exec(className || "");
-  //     return !inline && match ? (
-  //       <SyntaxHighlighter
-  //         style={customBlackTheme}
-  //         language={match[1]}
-  //         PreTag='div'
-  //         children={String(children).replace(/\n$/, "")}
-  //         {...props}
-  //       />
-  //     ) : (
-  //       <code className={className} {...props}>
-  //         {children}
-  //       </code>
-  //     );
-  //   },
-  // };
-
-
   return (
     <div className={`chat-container ${darkTheme ? 'dark-mode': ''}`}>
       <div className='chat-messages'>
