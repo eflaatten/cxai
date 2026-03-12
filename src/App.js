4c117eb5d8e0126a9c859091a85e1e264b1795f4
eflaatten <elliotflaatten@live.com>
2026-03-12 09:23:26 -0500

    endpoint

--- src/App.js
+++ src/App.js
@@ -34,8 +34,7 @@ function App() {
   const getEndpoint = () =>
     `https://cxf-executor-dev.cxfabric.io/restendpoint` +
   `?tenant_id=${process.env.REACT_APP_TENANT_ID}` +
-  `&flow_id=${process.env.REACT_APP_FLOW_ID}` +
-  `&targetUserId=${process.env.REACT_APP_USER_ID}`;
+  `&flow_id=${process.env.REACT_APP_FLOW_ID}`;
 
   const buildHeaders = () => ({
     "Content-Type": "application/json",
