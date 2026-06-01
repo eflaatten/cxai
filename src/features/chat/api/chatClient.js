cc04b9248f3df2e4590831b9d0f8dd36926cc6fb
eflaatten <elliotflaatten@live.com>
2026-06-01 10:38:19 -0500

    change troubleshooting flow url

--- src/features/chat/api/chatClient.js
+++ src/features/chat/api/chatClient.js
@@ -7,17 +7,17 @@ const OPEN_METEO_GEOCODING_ENDPOINT =
 //https://cxf-executor-dev.cxfabric.io/restendpoint?tenant_id=cus_QZ2vTHtqYrOmud&flow_id=69e5fde2-e20d-4048-9499-8665cc70a0a5&draft=true&targetUserId=auth0_67bdf583d7397dc4f217a8e0&displayExecutionLogs=true // weather.gov api
 //https://cxf-executor-dev.cxfabric.io/restendpoint?tenant_id=cus_QZ2vTHtqYrOmud&flow_id=34822cb4-a0dc-4893-9e50-0f90020b1de8&draft=true&targetUserId=auth0_67bdf583d7397dc4f217a8e0&displayExecutionLogs=true // open-meteo
 //https://cxf-executor-qa.cxfabric.io/restendpoint?tenant_id=bb40a7e5-3721-4bc9-b430-aa980a8e9918&flow_id=6ebddf47-3694-4336-90af-578f10d6cb6c&draft=true&targetUserId=auth0_6a0b9365f87fcdb1e3441c76&displayExecutionLogs=true
-const FLOW_WEATHER_ENDPOINT = "https://cxf-executor-dev.cxfabric.io/restendpoint?tenant_id=cus_QZ2vTHtqYrOmud&flow_id=69e5fde2-e20d-4048-9499-8665cc70a0a5&draft=true&targetUserId=auth0_67bdf583d7397dc4f217a8e0&displayExecutionLogs=true";
+const FLOW_WEATHER_ENDPOINT = "https://cxf-executor-dev.cxfabric.io/restendpoint?tenant_id=cus_QZ2vTHtqYrOmud&flow_id=69e5fde2-e20d-4048-9499-8665cc70a0a5&draft=true&targetUserId=*&displayExecutionLogs=true";
 const WEATHER_TOOL_NAME = "get_current_weather";
 
 // MAKO DEMO DYNAMO DB TOOL
-const DYNAMO_DB_RETRIEVAL_ENDPOINT = "https://cxf-executor-qa.cxfabric.io/restendpoint?tenant_id=1bdd5282-6602-4a6b-8ad6-a94f57c5fa2b&flow_id=14106d32-c3c7-4b02-971a-bf8ee0ce6f95&draft=true&targetUserId=auth0_6a1726b7b05e9a67c02b7af2&displayExecutionLogs=true";
+const DYNAMO_DB_RETRIEVAL_ENDPOINT = "https://cxf-executor-qa.cxfabric.io/restendpoint?tenant_id=1bdd5282-6602-4a6b-8ad6-a94f57c5fa2b&flow_id=14106d32-c3c7-4b02-971a-bf8ee0ce6f95&draft=true&targetUserId=*&displayExecutionLogs=true";
 const MAKO_DEMO_TOOL_NAME = "retrieve_gas_stations";
 const MAKO_STATION_ID_MIN = 1004;
 const MAKO_STATION_ID_MAX = 10010;
 
 // MAKO NETWORKS TROUBLESHOOTING TOOL
-const MAKO_TROUBLESHOOTING_ENDPOINT = "https://cxf-executor-qa.cxfabric.io/restendpoint?tenant_id=1bdd5282-6602-4a6b-8ad6-a94f57c5fa2b&flow_id=a4ecae0a-cc50-4d96-a896-225176c100f7&draft=true&targetUserId=auth0_6a1726b7b05e9a67c02b7af2&displayExecutionLogs=true";
+const MAKO_TROUBLESHOOTING_ENDPOINT = "https://cxf-executor-qa.cxfabric.io/restendpoint?tenant_id=1bdd5282-6602-4a6b-8ad6-a94f57c5fa2b&flow_id=d4d18fa7-45fc-4aad-ad65-d8c95e0c46ea&draft=true&targetUserId=*&displayExecutionLogs=true";
 const MAKO_TROUBLESHOOTING_TOOL_NAME = "get_mako_troubleshooting";
 
 const MAX_TOOL_ROUNDS = 3;
